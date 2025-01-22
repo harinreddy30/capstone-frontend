37fee07a3f0eac8ca2e5e1d6f0e49e8469b42397
Pmack0702 <meetpatel7026@gmail.com>
2025-01-21 19:02:47 -0800

    AuthSlice Created

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -12,7 +12,21 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         loginStart: (state) => {
-            state.loading = tru
-        }
-    }
-})
+            state.loading = true; // When the starts, set loading to true
+            state.error = null; // Clear previous error
+        },
+        loginSuccess: (state, action) => {
+            state.user = action.payload; // Store the user object returned from the backend.
+            state.loading = false; // Set loading to false when login is successful.
+        },
+        loginFailure: (state, action) => {
+            state.loading = false; // Set loading to false when login fails.
+            state.error = action.payload; // Store the error message.
+        },
+        logout: (state) => {
+            state.user = null; // Clear user data on logout.
+        },
+    },
+});
+
+// Export the actions so that we can dispatch them from components.
