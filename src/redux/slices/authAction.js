1bf81fd86c6a791406f42410849c2a6fd580f915
Pmack0702 <meetpatel7026@gmail.com>
2025-01-21 19:11:30 -0800

    AuthAction Created

--- src/redux/slices/authAction.js
+++ src/redux/slices/authAction.js
@@ -1,1 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { loginStart, loginSuccess, loginFailure } from '../slices/authSlice';
+import axios from 'axios';
+
+export const loginUser = ({ email, password }) => async (dispatch) => {
+    dispatch(loginStart());
+    try {
+        const response = await axios.post('/api/v1/login', {email, password});
+        dispatch(loginSuccess(response.data));
+        return response.data; // Return data to handle role-based redirection
+
+    } catch (error) {
+        dispatch(loginFailure(error.response.data.message || 'Login Failed'));
+        return null
+    }
+
+}
