8cd45ccb764df47ea3e01e75da8b167f9b696a11
Pmack0702 <meetpatel7026@gmail.com>
2025-01-22 08:52:40 -0800

    App.js Configured and Corrected

--- src/App.js
+++ src/App.js
@@ -1,41 +1,109 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PayrollManagement from './pages/Payroll/PayrollManagement';
-import EmployeeList from './pages/Payroll/EmployeeList';
-import PayrollGenerator from './pages/Payroll/PayrollGenerator';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux"; // Use to manage application State, token and yser data\
+
 import Login from './pages/Auth/Login';
 
+import EmployeeDashboard from './Dashboard/EmployeeDashboard/EmployeeDashboard'
+import HRDashboard from './Dashboard/HRDashboard/HRDashboard'
+import ManagerDashboard from './Dashboard/ManagerDashboard/ManagerDashboard'
+import PayrollDashboard from './Dashboard/PayrollDashboard/PayrollDashboard'
+import AdminDashboard from './Dashboard/AdminDashboard/AdminDashboard'
+
+// A private route wrapper to protect routes based on authentication
+// Implement RBAC to implement authentication and authorization
+const ProtectedRoute = ({ children, authorizedRoles }) => {
+  const { token , user } = useSelector((state) => state.auth) // auth is the name of the Slice that contains token and user
+
+  if(!token){
+    return <Navigate to='/login'/>
+  }
+
+  if(authorizedRoles && !authorizedRoles.includes(user.role)){
+    return <Navigate to='/' /> // Redirect if the user doesnt have any roles
+  }
+
+  return children;
+
+}
+
+// UseEffect is used to remove the token from localStorage, if the token is missing
+const App = () => {
+  const { token } = useSelector((state) => state.auth);
+
+  // Automaticaly logout if the token is missing
+  useEffect(() => {
+    if(!token){
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+}
 function App() {
   return (
       <Router>
         <Routes>
+          {/* Public Routes */}
           <Route path='/login' element={<Login />} /> 
-          {/* Other Public and Protected Routes */}
-          {/* Payroll Module */}
-          {/* <Route
-            path="/payroll/management"
-            element={
-                <PayrollManagement />
-            }
+
+          {/* Protected Routes */}
+
+          {/* Employee Protected Routes */}
+          <Route 
+            path="/employee/dashboard" 
+            element = {
+              <ProtectedRoute authorizedRoles={["Employee"]}>
+                <EmployeeDashboard />
+              </ProtectedRoute>
+            } 
           />
-          <Route
-            path="/payroll/employeelist"
-            element={
-                <EmployeeList />
-            }
+
+          {/* HR Protected Routes */}
+          <Route 
+            path="/HR/dashboard" 
+            element = {
+              <ProtectedRoute authorizedRoles={["HR"]}>
+                <HRDashboard />
+              </ProtectedRoute>
+            } 
           />
-          <Route
-            path="/payroll/generator"
-            element={
-                <PayrollGenerator />
-            }
-          /> */}
+
+          {/* Manager Protected Routes */}
+          <Route 
+            path="/Manager/dashboard" 
+            element = {
+              <ProtectedRoute authorizedRoles={["Manager"]}>
+                <ManagerDashboard />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Payroll Protected Routes */}
+          <Route 
+            path="/Payroll/dashboard" 
+            element = {
+              <ProtectedRoute authorizedRoles={["PayrollManager"]}>
+                <PayrollDashboard />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Admin Protected Routes */}
+          <Route 
+            path="/Admin/dashboard" 
+            element = {
+              <ProtectedRoute authorizedRoles={["Admin"]}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            } 
+          />
+
+          {/* Redirect unknown routes */}
+          <Route path="*" element={<Navigate to="/login" />} />
+          
         </Routes>
       </Router>
   );
 }
 
-console.log("PayrollManagement:", PayrollManagement);
-console.log("EmployeeList:", EmployeeList);
-console.log("PayrollGenerator:", PayrollGenerator);
+
 export default App;
