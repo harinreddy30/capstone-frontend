39f9da9e358a9b765a5c2c3d0c3a7b607d33e82e
Pmack0702 <meetpatel7026@gmail.com>
2025-01-24 08:28:38 -0800

    .

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -16,6 +16,7 @@ apiClient.interceptors.request.use((config) => {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
+    
 }, (error) => {
     // Handle request error
     return Promise.reject(error);
